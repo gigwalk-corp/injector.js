cd4a6a861f0687cec1f56edc3e326e78aa0b6cae
Steven Bassett <steven.j.bassett@gmail.com>
2016-01-13 17:32:36 -0800

    Update: Add error checks for safer injections

--- src/injectIntoContext.js
+++ src/injectIntoContext.js
@@ -1,18 +1,31 @@
+import 'babel-polyfill';
+export class InjectorContextError extends Error {}
 
 export default function injectIntoContext(injector, Component) {
+    if (!Component.hasOwnProperty('childContextTypes')) {
+        throw new InjectorContextError('Expected childContextTypes to be defined on the container element');
+    }
+
     Object.defineProperty(Component.prototype, 'getChildContext', {
         writable: false,
         configurable: false,
         enumerbale: false,
         value: function getChildContext() {
             const context = {};
-            for (const key in Component) {
-                if (Component[key] === 'inject') {
-                    context[key] = injector.getInstance(key);
-                }
+            const injectables = Object.entries(Component)
+                .filter(([, value]) => value === 'inject');
+            if (!injectables.length) {
+                throw new InjectorContextError('Exepected static strings to be defined on the container class');
             }
+
+            injectables
+                .forEach(([key]) => {
+                    context[key] = injector.getInstance(key);
+                });
 
             return context;
         }
     });
+
+    return Component;
 }
